386e9e1f44d3a525d555f59005685aa7388f7377
Mathieu PINSON <mpinson@e1r5p5.42.fr>
2019-10-07 22:46:29 +0200

    css

--- src/client/containers/app.js
+++ src/client/containers/app.js
@@ -3,8 +3,7 @@ import { connect } from 'react-redux'
 import { BrowserRouter as Router, Switch, Route, Link, withRouter, HashRouter } from "react-router-dom";
 import { homedir } from 'os';
 
-
-function Makebord() {
+function Makebord(c) {
     var container = []
     var key = 1
     var x = 0;
@@ -13,6 +12,24 @@ function Makebord() {
         const y = 0;
         while (y < 20) {
             container.push(<div className="caseBord" key={key} col={x} row={y}></div>)
+            y++;
+            key++;
+        }
+        x++;
+    }
+
+    return (container)
+}
+
+function SpectreSolo(c) {
+    var container = []
+    var key = 1
+    var x = 0;
+
+    while (x < 10) {
+        const y = 0;
+        while (y < 20) {
+            container.push(<div className="case-spectre-solo" key={key} col={x} row={y}></div>)
             y++;
             key++;
         }
@@ -96,7 +113,7 @@ function Comp(props) {
 function Solo() {
     return (
         <div id="container-party-solo">
-            <div className="content">
+            <div className="content-bord-solo">
                 <div id="bord">
                     <Makebord />
                 </div>
@@ -107,6 +124,16 @@ function Solo() {
                         <MakeNewPiece />
                     </div>
                 </div>
+                <p>your score :</p>
+                <img id="title-img" src="./assets/images/title.png"></img>
+                <div id="container-spectre-solo">
+                    <div id="bord_display">
+                        <div id="spectre-border-container">
+                            <SpectreSolo />
+                        </div>
+                    </div>
+                </div>
+
             </aside>
 
         </div>
