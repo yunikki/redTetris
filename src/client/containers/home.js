3e316eecb4df468f316cc158988643f0db5a9502
Mathieu PINSON <mpinson@e1r8p6.42.fr>
2019-10-16 13:17:11 +0200

    ok

--- src/client/containers/home.js
+++ src/client/containers/home.js
@@ -2,15 +2,15 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link, withRouter, HashRouter } from "react-router-dom";
 import reducer from '../reducers'
 import { connect } from 'react-redux';
-import { chargePageSolo, inputYourName, inputYourNameRoom, chargeLobby, searchingRooms, saveSearch} from '../components/action'
-import { dataChargeLobby } from '../actions'
-import { dataCreateRoom, getRoomInfos} from '../actions/server'
+import { chargePageSolo, inputYourName, inputYourNameRoom, chargeLobby, searchingRooms, saveSearch } from '../components/action'
+import { dataChargeLobby, dataChangeInputName } from '../actions'
+import { dataCreateRoom, getRoomInfos } from '../actions/server'
 
 function notChargeLobby() {
     console.log('test')
 }
 
-function Home({ pageSolo, inputYourName, inputYourNameRoom, state, chargeLobby, startSearch}) {
+function Home({ pageSolo, inputYourName, inputYourNameRoom, state, chargeLobby, startSearch }) {
     return (
         <Router>
             <div id="menu">
@@ -25,7 +25,7 @@ function Home({ pageSolo, inputYourName, inputYourNameRoom, state, chargeLobby, 
                     </div>
                     <div id="creat-party">
                         <p>Create Game</p>
-                        <input className="input-creat" id="your-name-creat-room" type="text" placeholder="Your Name" onChange={inputYourName} />
+                        <input className="input-creat" value={state.inputName} id="your-name-creat-room" type="text" placeholder="Your Name" onChange={(e) => startSearch(e)} />
                         <input className="input-creat" id="name-room-creat-room" type="text" placeholder="Room Name" onChange={inputYourNameRoom} />
                         <br></br>
                         <Link id="button-start-room" className="btn" to={!state.runRoom ? "" : "/#" + state.inputNameRoom + '[' + state.inputName + ']'} onClick={state.runRoom ? () => chargeLobby(state) : notChargeLobby} disabled={!state.runRoom}>Create Room</Link>
@@ -33,13 +33,13 @@ function Home({ pageSolo, inputYourName, inputYourNameRoom, state, chargeLobby, 
 
 
                 </div>
-                <h3>Join Game!</h3> 
-                <input className="input-creat" id="search-party" type="text" placeholder="Search Room" onChange={(e) => startSearch(e)}/>
+                <h3>Join Game!</h3>
+                <input className="input-creat" value={state.inputName} id="search-party" type="text" placeholder="entre un nom avent d aller dans un room" onChange={(e) => startSearch(e)} />
                 <div id="list-room">
                     <div id="list-room-line-first">
                         <div className="name-list">Room name</div>
                         <div className="creat-list">Creator</div>
-                        <div className="player-list">Players</div> 
+                        <div className="player-list">Players</div>
                     </div>
                 </div>
             </div>
@@ -54,15 +54,14 @@ const mapStateToProps = (state, ownProps) => ({
 
 const mapDispatchToProps = (dispatch) => {
     return ({
-        pageSolo: chargePageSolo(dispatch),
-        inputYourName: inputYourName(dispatch),
         inputYourNameRoom: inputYourNameRoom(dispatch),
         chargeLobby: (state) => {
             dispatch(dataCreateRoom(state))
             dispatch(dataChargeLobby())
         },
         startSearch: (e) => {
-            dispatch(saveSearch(dispatch, e)),
+            dispatch(dataChangeInputName(e.target.value))
+            dispatch(saveSearch(dispatch, e))
             dispatch(getRoomInfos())
         }
     });
